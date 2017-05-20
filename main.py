269bb467717dec6a86735e6eaf2f5c09fb0bd920
Srinivasa Karthic Ponguru <karthic.psa@gmail.com>
2017-05-20 15:18:51 -0500

    web app done with '/' as handler. Need to fix to '/rot13'.

--- main.py
+++ main.py
@@ -17,36 +17,39 @@ import webapp2
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
+
+
 
 
 def render_str(template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
-class BHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render(self, template, **kw):
-        self.write(render_str(template, **kw))
-
-class Rot13(BHandler):
+class Rot13(BaseHandler):
     def get(self):
-        self.render("main_page.html")
+        self.render('main_page.html')
 
     def post(self):
         rot13 = ''
         text = self.request.get('text')
         if text:
-            rot13=text.encode('rot13')
-        self.render('main_page.html',text=rot13)
+            rot13 = text.encode('rot13')
 
-class MainPage(Handler):
-    def get(self):
+        self.render('main_page.html', text = rot13)
+
+# class MainPage(BHandler):
+#     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
         # self.response.write(form)
-        self.render("main_page.html")
+        # self.render("main_page.html")
 
 # class TestHandler(webapp2.RequestHandler):
 #     def post(self):
@@ -55,7 +58,5 @@ class MainPage(Handler):
 #         self.response.headers['Content-Type'] = 'text/plain'
 #         self.response.out.write(self.request)
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/test/rot13', Rot13)
-], debug=True)
+app = webapp2.WSGIApplication([('/', Rot13)],
+    debug=True)
