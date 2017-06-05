b274ba14d53820c0ea357b4f6b8f23317761c664
Srinivasa Karthic Ponguru <karthic.psa@gmail.com>
2017-06-05 18:29:38 -0500

    Comments edit.

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-  # Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import re
 from string import letters
@@ -60,6 +46,3 @@ class Rot13(BHandler):
 
 
 app = webapp2.WSGIApplication([('/rot13', Rot13)], debug=True)
-#app = webapp2.WSGIApplication([('/rot13', Rot13),
-#                              ],
-#                              debug=True)
