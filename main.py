f269e990b75cd3b1d50cb4b7f8ab0c6974e07804
Srinivasa Karthic Ponguru <karthic.psa@gmail.com>
2017-06-05 19:14:51 -0500

    Final Rot13 web app with the required HTML form.

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import os
-import re
-from string import letters
-
 import webapp2
 import jinja2
-
-from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
 
-
-
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
-
 
 class BHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
@@ -42,7 +33,5 @@ class Rot13(BHandler):
             rot13 = text.encode('rot13')
 
         self.render('main_page.html', text = rot13)
-
-
 
 app = webapp2.WSGIApplication([('/rot13', Rot13)], debug=True)
